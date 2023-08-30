c069655c633723e5a63c4c648e68b57b6e247beb
Igor <igor.dom.com@gmail.com>
2023-08-30 21:50:24 +0200

    Improve employees page

--- php/src/employees.php
+++ php/src/employees.php
@@ -1,11 +1,26 @@
 <?php
-
+$name_filter = $_GET['name'] ?? "";
 include_once 'db.php';
 
-$statement = $pdo->query("SELECT 'Привет, дорогой пользователь MySQL!' AS _message FROM DUAL");
-$row = $statement->fetch(PDO::FETCH_ASSOC);
+$statement = $pdo->query("SELECT
+        e.emp_no,
+        CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+        d.dept_name AS department,
+        cte.title,
+        e.birth_date,
+        e.gender
+    FROM
+        employees e
+        JOIN current_title_emp cte ON cte.emp_no = e.emp_no
+        JOIN current_dept_emp cde ON cde.emp_no = e.emp_no
+        JOIN departments d ON cde.dept_no = d.dept_no
+    WHERE CONCAT(e.first_name, ' ', e.last_name) LIKE :name_filter
+    LIMIT 100");
+$statement->execute(['name_filter' => '%'. $name_filter .'%']);
+$employees = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-// var_dump($_GET)
+// var_dump($row);
+
 
 //TODO:
 // - show 100 current employees with columns emp_no, full_name, deparment, title
@@ -27,45 +42,38 @@ $row = $statement->fetch(PDO::FETCH_ASSOC);
 <body>
     <main class="container">
         <form method="GET">
-            <input type="search" id="search" name="name" placeholder="Search" />
+            <input type="search" id="search" name="name" value="<?= $name_filter ?>"
+                placeholder="Enter employees name" />
             <input type="submit" value="Submit" />
         </form>
         <table role="grid">
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Heading</th>
-                    <th scope="col">Heading</th>
-                    <th scope="col">Heading</th>
-                    <th scope="col">Heading</th>
-                    <th scope="col">Heading</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Department</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Birth date</th>
+                    <th scope="col">Gender</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                </tr>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                </tr>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                    <td>Cell</td>
-                </tr>
+                <?php foreach($employees as &$row) {
+                    $template = <<<HTML
+                        <tr>
+                            <th scope="row">$row[emp_no]</th>
+                            <td>$row[full_name]</td>
+                            <td>$row[department]</td>
+                            <td>$row[title]</td>
+                            <td>$row[birth_date]</td>
+                            <td>$row[gender]</td>
+                        </tr>
+                    HTML;
+                    
+                    echo $template;
+                }
+                        
+                ?>
             </tbody>
         </table>
     </main>
