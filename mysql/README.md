2d4b35e3f9e00e7a1cfcb3ef0449994a85886af6
Oleksii Berezovskyi <alex.b.2658@gmail.com>
2023-09-17 01:45:18 +0300

    Tweak example script of running sql query from shell

--- mysql/README.md
+++ mysql/README.md
@@ -1,10 +1,15 @@
 Dump:
-
-```sh 
+```sh
 docker exec -i mysql-db-1 sh -c 'exec mysqldump -u root -p"$MYSQL_ROOT_PASSWORD" employees' > employees.sql
 ```
 
 Restore:
 ```sh
-docker exec -i mysql-db-1 sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD"' <  employees.sql
+docker exec -i mysql-db-1 sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE employees;"'
+docker exec -i mysql-db-1 sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" employees' <  employees.sql
+```
+
+Launch script inside Docker container:
+```sh
+docker exec -i mysql-db-1 sh < script.sh
 ```
--- mysql/cen.sh
+++ mysql/cen.sh
@@ -1,14 +1,20 @@
+#!/bin/sh
 
-SQL="
+query=$(cat <<-SQL
+-- number of current employees
 SELECT SUM(1) FROM
     current_dept_emp cde
 WHERE
     cde.to_date = '9999-01-01';
-"
+SQL
+)
 
+# Avoid warning of passing password as a CLI option
 # https://unix.stackexchange.com/a/334971
-export MYSQL_PWD=$MYSQL_ROOT_PASSWORD
+# `export` делает переменную доступной для комманд запущеных внутри скрипта
+export MYSQL_PWD="$MYSQL_ROOT_PASSWORD"
 
-NUM=$(echo $SQL | mysql -uroot -D employees -s)
+# Note that $query inclosed into double quotes
+result=$(echo "$query" | mysql -s -uroot employees)
 
-echo Number of current employees $NUM
+echo "Number of current employees $result"
